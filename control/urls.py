44d4f0811cfea8045dffd6dbe119a64225b68d54
Noa Cohen <nono9600@gmail.com>
2018-05-18 18:02:11 +0300

    first commit from pi!

--- control/urls.py
+++ control/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^send_command/$', views.run_code, name='run_code'),
 ]
--- control/views.py
+++ control/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
+from .handlers.quadcopter_control import Drone 
 
 # Create your views here.
+
+drone = None
 
 def index(request):
     template = loader.get_template('control/index.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def run_code(request):
+    global drone
+    command = request.GET.get('command', None)
+    if command == 'start' and not drone:
+        drone = Drone()
+        drone.arm()
+        arm = True
+    if command == 'stop' and drone:
+        drone.disarm()
+        drone = None
+        arm = False
+    error = None
+    try:
+        if drone and command not in ["start", "stop"]:
+            drone.run_command(command)
+    except NameError as err:
+        error = 'Drone not initialized: ' + str(err)
+    response = {
+            'success': True,
+            'error': error
+        }
+    return JsonResponse(response)
