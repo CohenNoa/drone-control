a01f8bca1d40b32772a080fd6fb487473e1d6f64
= <=>
2018-06-08 13:22:16 +0300

    fixed import GPIO error

--- control/handlers/attitude_handler.py
+++ control/handlers/attitude_handler.py
@@ -3,8 +3,8 @@ import time
 running_on_computer = False
 try:
     import RPi.GPIO as GPIO
-except RuntimeError:
-    running_on_computer = True
+except (RuntimeError, ImportError):
+    debug_mode = True
     print("GPIO module can only be run on a Raspberry Pi!")
 
 timeout = 3  # seconds
@@ -13,7 +13,7 @@ frequency = 50  # Hz
 
 class AttitudeHandler(object):
     def __init__(self, pitch, yaw, roll, throttle):
-        if not running_on_computer:
+        if not debug_mode:
             self.pitch_pin = pitch
             self.yaw_pin = yaw
             self.roll_pin = roll
@@ -47,14 +47,14 @@ class AttitudeHandler(object):
         return (percentage / 20) + 5
 
     def default_position(self):
-        if not running_on_computer:
+        if not debug_mode:
             self.pitch_pwm.start(self.middle_duty_cycle)
             self.yaw_pwm.start(self.middle_duty_cycle)
             self.roll_pwm.start(self.middle_duty_cycle)
             self.throttle_pwm.start(self.min_duty_cycle)
 
     def arm(self):
-        if not running_on_computer:
+        if not debug_mode:
             self.pitch_pwm.start(self.min_duty_cycle)
             self.yaw_pwm.start(self.max_duty_cycle)
             self.roll_pwm.start(self.min_duty_cycle)
@@ -63,7 +63,7 @@ class AttitudeHandler(object):
             self.default_position()
 
     def disarm(self):
-        if not running_on_computer:
+        if not debug_mode:
             self.pitch_pwm.start(self.min_duty_cycle)
             self.yaw_pwm.start(self.min_duty_cycle)
             self.roll_pwm.start(self.max_duty_cycle)
@@ -73,7 +73,7 @@ class AttitudeHandler(object):
 
     def change_channel_duty_cycle(self, name, percent):
         print("changing channel with name \"" + name + "\" to " + str(percent) + "%")
-        if not running_on_computer:
+        if not debug_mode:
             duty_cycle = self.get_duty_cycle(percent)
             if name == "pitch":
                 self.pitch_pwm.start(duty_cycle)
