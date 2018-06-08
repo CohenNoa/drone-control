4a725315c720bf2fe13aa302d0323afc2c58f847
Noa Cohen <nono9600@gmail.com>
2018-06-08 20:50:35 +0300

    .

--- control/handlers/attitude_handler.py
+++ control/handlers/attitude_handler.py
@@ -1,6 +1,6 @@
 import time
 
-running_on_computer = False
+debug_mode= False
 try:
     import RPi.GPIO as GPIO
 except (RuntimeError, ImportError):
@@ -12,8 +12,13 @@ frequency = 50  # Hz
 
 
 class AttitudeHandler(object):
-    def __init__(self, pitch, yaw, roll, throttle):
+    def __init__(self, return_home, mode, pitch, yaw, roll, throttle):
         if not debug_mode:
+            self.modes_names = {10: "Stabilize", 30: "FailSafe 1", 50: "Intellegent Orientation Control", 70: "FailSafe 2", 90: "GPS"}
+            self.modes = [10, 30, 50, 70, 90]
+
+            self.return_home_pin = return_home
+            self.mode_pin = mode
             self.pitch_pin = pitch
             self.yaw_pin = yaw
             self.roll_pin = roll
@@ -21,24 +26,34 @@ class AttitudeHandler(object):
 
             GPIO.setmode(GPIO.BCM)
 
+            GPIO.setup(self.return_home_pin, GPIO.OUT)
+            GPIO.setup(self.mode_pin, GPIO.OUT)
             GPIO.setup(self.pitch_pin, GPIO.OUT)
             GPIO.setup(self.yaw_pin, GPIO.OUT)
             GPIO.setup(self.roll_pin, GPIO.OUT)
             GPIO.setup(self.throttle_pin, GPIO.OUT)
 
+            self.return_home_pwm = GPIO.PWM(self.return_home_pin, frequency)
+            self.mode_pwm = GPIO.PWM(self.mode_pin, frequency)
             self.pitch_pwm = GPIO.PWM(self.pitch_pin, frequency)
             self.yaw_pwm = GPIO.PWM(self.yaw_pin, frequency)
             self.roll_pwm = GPIO.PWM(self.roll_pin, frequency)
             self.throttle_pwm = GPIO.PWM(self.throttle_pin, frequency)
 
+            self.return_home_default = self.get_duty_cycle(0)
+            self.mode_default = self.get_duty_cycle(self.modes[0])
             self.min_duty_cycle = self.get_duty_cycle(0)
             self.middle_duty_cycle = self.get_duty_cycle(50)
             self.max_duty_cycle = self.get_duty_cycle(100)
 
+            self.return_home_pwm.start(self.return_home_default)
+            self.mode_pwm.start(self.mode_default)
             self.pitch_pwm.start(self.middle_duty_cycle)
             self.yaw_pwm.start(self.middle_duty_cycle)
             self.roll_pwm.start(self.middle_duty_cycle)
             self.throttle_pwm.start(self.min_duty_cycle)
+
+
 
         print("Setup complete")
 
@@ -55,7 +70,7 @@ class AttitudeHandler(object):
 
     def arm(self):
         if not debug_mode:
-            self.pitch_pwm.start(self.min_duty_cycle)
+            self.pitch_pwm.start(self.max_duty_cycle)
             self.yaw_pwm.start(self.max_duty_cycle)
             self.roll_pwm.start(self.min_duty_cycle)
             self.throttle_pwm.start(self.min_duty_cycle)
@@ -64,7 +79,7 @@ class AttitudeHandler(object):
 
     def disarm(self):
         if not debug_mode:
-            self.pitch_pwm.start(self.min_duty_cycle)
+            self.pitch_pwm.start(self.max_duty_cycle)
             self.yaw_pwm.start(self.min_duty_cycle)
             self.roll_pwm.start(self.max_duty_cycle)
             self.throttle_pwm.start(self.min_duty_cycle)
@@ -76,6 +91,7 @@ class AttitudeHandler(object):
         if not debug_mode:
             duty_cycle = self.get_duty_cycle(percent)
             if name == "pitch":
+                duty_cycle = self.get_duty_cycle(100-percent)
                 self.pitch_pwm.start(duty_cycle)
             if name == "yaw":
                 self.yaw_pwm.start(duty_cycle)
@@ -83,3 +99,18 @@ class AttitudeHandler(object):
                 self.roll_pwm.start(duty_cycle)
             if name == "throttle":
                 self.throttle_pwm.start(duty_cycle)
+
+    def change_mode(self, mode):
+        duty_cycle = self.get_duty_cycle(self.modes[mode])
+        print("changing mode to " + self.modes_names[self.modes[mode]])
+        if not debug_mode:
+            self.mode_pwm.start(duty_cycle)
+
+    def change_return_home(self, return_home):
+        if not debug_mode:
+            if return_home:
+                self.return_home_pwm.start(self.get_duty_cycle(100))
+                print("returning home")
+            else:
+                self.return_home_pwm.start(self.get_duty_cycle(0))
+                print("business as ususal")
--- control/handlers/quadcopter_control.py
+++ control/handlers/quadcopter_control.py
@@ -5,7 +5,7 @@ from .attitude_handler import AttitudeHandler as Attitude
 
 class Drone(object):
     def __init__(self):
-        self.attitude = Attitude(12, 16, 20, 21)
+        self.attitude = Attitude(7, 1, 12, 16, 20, 21)
         self.enable_controls = True
 
     def run_command(self, command):
@@ -13,6 +13,12 @@ class Drone(object):
         if args[0] == "attitude" and self.enable_controls:
             if args[1] in ["pitch", "roll", "yaw", "throttle"] and 100 >= float(args[2]) >= 0:
                 self.attitude.change_channel_duty_cycle(args[1], int(float(args[2])))
+        if args[0] == "mode":
+            if args[1] in ["0","1","2","3","4"]:
+                self.attitude.change_mode(int(args[1]))
+        if args[0] == "return-home":
+            if args[1] in ["0", "1"]:
+                self.attitude.change_return_home(True if args[1] is "1" else False)
 
     def arm(self):
         print("Arm")
